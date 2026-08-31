2d8250ec20329333eb486603e26a60d4770d5d16
Navindu Gayeshka <108661960+NavinduG@users.noreply.github.com>
2026-08-31 15:55:50 +0000

    feat(rabbitmq): add RabbitMQ configuration with JSON message converter

--- services/notification-service/src/main/java/com/stockora/notification/config/RabbitMQConfig.java
+++ services/notification-service/src/main/java/com/stockora/notification/config/RabbitMQConfig.java
@@ -1,5 +1,27 @@
 package com.stockora.notification.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
+
     public static final String ORDER_CREATED_QUEUE = "order.created.queue";
+
+    @Bean
+    public JacksonJsonMessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory, JacksonJsonMessageConverter converter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(converter);
+        return factory;
+    }
 }
