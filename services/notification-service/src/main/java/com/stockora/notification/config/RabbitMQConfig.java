308add90de664bc5b93eff3d1d9259b6d7456cab
Navindu Gayeshka <108661960+NavinduG@users.noreply.github.com>
2026-09-01 09:03:05 +0000

    fix(rabbitmq): correct import statement for SimpleRabbitListenerContainerFactory

--- services/notification-service/src/main/java/com/stockora/notification/config/RabbitMQConfig.java
+++ services/notification-service/src/main/java/com/stockora/notification/config/RabbitMQConfig.java
@@ -1,7 +1,7 @@
 package com.stockora.notification.config;
 
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
